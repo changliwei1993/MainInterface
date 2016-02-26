49786642d8ba8e00d6e87eed3d5fe27fa72f0d31
韦昌力 <774145959@qq.com>
2016-02-26 09:14:29 +0800

    出现镜像

---
+++ app/src/main/java/dream/com/mianinterface/SideImageView.java
@@ -0,0 +1,107 @@
+package dream.com.mianinterface;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.PixelFormat;
+import android.os.Handler;
+import android.os.Vibrator;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.Gravity;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+
+/**
+ * Created by Arthur on 2016/2/25 16 : 06.
+ */
+public class SideImageView extends ImageView {
+
+
+    private Context context;
+    private RelativeLayout _root;
+    private String TAG="SideImageView";
+    private boolean isDrag = false;
+    private Vibrator mVibrator;
+    private WindowManager mWindowManager;
+    private WindowManager.LayoutParams mWindowLayoutParams;
+    private Bitmap mDragBitmap;
+    private int mDownX;
+    private int mDownY;
+    private ImageView mDragImageView;
+
+    public SideImageView(Context context) {
+        super(context);
+    }
+
+    public void set_root(RelativeLayout _root) {
+        this._root = _root;
+    }
+
+    public SideImageView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public SideImageView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+
+    }
+
+    private Handler mHandler = new Handler();
+    private Runnable mLongClickRunnable = new Runnable() {
+
+        @Override
+        public void run() {
+            isDrag = true;
+            mVibrator.vibrate(50);
+            setVisibility(View.INVISIBLE);
+            createDragImage(mDragBitmap, mDownX, mDownY);
+        }
+    };
+    public void setContext(Context context) {
+        this.context = context;
+        mVibrator = (Vibrator)this.context.getSystemService(context.VIBRATOR_SERVICE);
+        mWindowManager = (WindowManager)this.context.getSystemService(Context.WINDOW_SERVICE);
+    }
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        switch (event.getAction()){
+            case MotionEvent.ACTION_DOWN:
+                setDrawingCacheEnabled(true);
+                mDragBitmap = Bitmap.createBitmap(getDrawingCache());
+                destroyDrawingCache();
+                mDownX = (int) event.getRawX();
+                mDownY = (int) event.getRawX();
+                mHandler.postDelayed(mLongClickRunnable, 1000);
+                Log.d(TAG,"ACTION_DOWN");
+                break;
+            case MotionEvent.ACTION_MOVE:
+                Log.d(TAG,"ACTION_MOVE");
+                break;
+            case MotionEvent.ACTION_UP:
+                Log.d(TAG,"ACTION_UP");
+                break;
+        }
+        return true;
+    }
+
+    private void createDragImage(Bitmap bitmap, int downX , int downY){
+        mWindowLayoutParams = new WindowManager.LayoutParams();
+        mWindowLayoutParams.format = PixelFormat.TRANSLUCENT;
+        mWindowLayoutParams.gravity = Gravity.TOP | Gravity.LEFT;
+        mWindowLayoutParams.x = downX;
+        mWindowLayoutParams.y = downY;
+        mWindowLayoutParams.alpha = 0.55f;
+        mWindowLayoutParams.width = WindowManager.LayoutParams.WRAP_CONTENT;
+        mWindowLayoutParams.height = WindowManager.LayoutParams.WRAP_CONTENT;
+        mWindowLayoutParams.flags = WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE
+                | WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE ;
+        mDragImageView = new ImageView(getContext());
+        mDragImageView.setImageBitmap(bitmap);
+        mWindowManager.addView(mDragImageView, mWindowLayoutParams);
+    }
+
+
+}
